251dffc3ad10938ba9f7cfd8e912eba5fdc0c754
Bascior <62135873+Bascior@users.noreply.github.com>
2022-09-21 21:37:16 +0200

    Date picker
    Selecting a date on bootstrap datepicker by sending keys to the input selecting a date from presented bootstrap values.

---
+++ 5_Date picker.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.keys import Keys
+
+'''
+** Date picker **
+
+1. Send value to the input
+2. Clear the input
+3. Choose today on the bootstrap date picker
+3. Clear the input
+5. Choose a day on the bootstrap date picker
+
+'''
+
+page = 'https://demo.seleniumeasy.com/bootstrap-date-picker-demo.html'
+
+# Variables
+date_sample = '29.08.2022'
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.implicitly_wait(15)
+browser.get(page)
+
+# 1. Send value to the input
+datepicker_input = browser.find_element(By.CLASS_NAME, 'form-control')
+datepicker_input.send_keys(date_sample)
+datepicker_input.send_keys(Keys.ENTER)
+
+time.sleep(3)
+
+# 2. CLear the input
+datepicker_input.clear()
+
+time.sleep(3)
+
+# 3. Choose today on the bootstrap date picker
+datepicker_input.click()
+today_date = browser.find_element(By.CLASS_NAME, 'today')
+today_date.click()
+
+time.sleep(3)
+
+# 4. CLear the input
+datepicker_input.clear()
+
+time.sleep(3)
+
+# 5. Choose a day on the bootstrap date picker
+datepicker_button = browser.find_element(By.CLASS_NAME, 'input-group-addon').click()
+day = browser.find_elements(By.TAG_NAME, 'td')[0].click()       # Choose the first day on presented bootstap
+
+
+
